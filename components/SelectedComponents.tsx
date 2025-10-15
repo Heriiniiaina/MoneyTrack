373e6d69caa331e99ad5fb0f7d2df9ff3efab547
Heriniaina Ramamonjitiana <heriniainaramamonjitiana@gmail.com>
2025-10-15 03:55:51 -0400

    select

---
+++ components/SelectedComponents.tsx
@@ -0,0 +1,71 @@
+import { colors } from "@/Constants/Color";
+import { capitalize, getFontSize } from "@/Constants/utils";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import React, { JSX, SetStateAction } from "react";
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const {width} = Dimensions.get("window")
+export type SelectedType = {
+  name: string;
+  value: string;
+};
+
+type Props = {
+  item: SelectedType[],
+  setSelected:React.Dispatch<SetStateAction<string>>,
+  selected:string
+ 
+};
+const iconSize = width < 400 ? width * 0.03 : 0.05;
+export const getIconName = (name: string): JSX.Element => {
+  if (name == "health")
+    return <FontAwesome name="heartbeat" size={iconSize} style={style.icon} />;
+  if (name == "shopping")
+    return <AntDesign name="shop" size={iconSize} style={style.icon} />;
+  if (name == "bill")
+    return <FontAwesome name="money" size={iconSize} style={style.icon} />;
+  return <AntDesign name="heart" size={iconSize} style={style.icon} />;
+};
+
+const SelectedComponents = ({ item, setSelected, selected }: Props) => {
+    const handleClick = (name:string)=>{
+        setSelected(name)
+        console.log(selected)
+    }
+  return (
+    <View style={style.container}>
+      {item.map((it, index) => {
+        const isSelected = it.name === selected
+        return (
+            <TouchableOpacity key={index} style={[style.iconContain, {borderColor:isSelected ? colors.primary : colors.textColor}]} onPress={()=>handleClick(it.name)}>
+              <View style={{alignItems:"center"}}>
+                {getIconName(it.name)}
+                <Text style={{fontSize:getFontSize(width, "min")- 3, color:isSelected ? colors.primary : colors.textColor}}>{capitalize(it.name)}</Text>
+              </View>
+            </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 20,
+    padding:10
+  },
+  iconContain: {
+    height: iconSize + 25,
+    width: iconSize + 25,
+    borderWidth: 1,
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:10
+  },
+  icon: {
+    color: colors.textColor,
+  },
+});
+export default SelectedComponents;
--- pages/AddBudgetPage.tsx
+++ pages/AddBudgetPage.tsx
@@ -1,25 +1,89 @@
-import TitleTransctionComponent from '@/components/TitleTransctionComponent'
-import { colors } from '@/Constants/Color'
-import React from 'react'
-import { Dimensions, StyleSheet, View } from 'react-native'
+import SelectedComponents, {
+  SelectedType,
+} from "@/components/SelectedComponents";
+import TitleTransctionComponent from "@/components/TitleTransctionComponent";
+import { colors } from "@/Constants/Color";
+import { getFontSize } from "@/Constants/utils";
+import React, { useState } from "react";
+import { Dimensions, StyleSheet, Text, TextInput, View } from "react-native";
 
-type Props = {}
+type Props = {};
 
-const {width} = Dimensions.get("window")
+const { width, height } = Dimensions.get("window");
+const categoryList: SelectedType[] = [
+  {
+    name: "health",
+    value: "health",
+  },
+  {
+    name: "transport",
+    value: "transport",
+  },
+  {
+    name: "shopping",
+    value: "shopping",
+  },
+  {
+    name: "health",
+    value: "health",
+  },
+  {
+    name:"others",
+    value:"others"
+  }
+];
 const AddBudgetPage = (props: Props) => {
+  const [category, setCategory] = useState<string>("");
   return (
     <View style={style.container}>
-       <View>
-            <TitleTransctionComponent route={"/(tabs)/budget"} text='Add budget' width={width}/>
-       </View>
+      <View>
+        <TitleTransctionComponent
+          route={"/(tabs)/budget"}
+          text="Add budget"
+          width={width}
+        />
+        <View style={style.input}>
+          <Text
+            style={{
+              fontSize: getFontSize(width, "other") - 2,
+              color: colors.textColor,
+            }}
+          >
+            Amount limit
+          </Text>
+          <TextInput
+            style={style.textInput}
+            placeholder="Amount"
+            placeholderTextColor={colors.placeholder}
+          />
+        </View>
+        <View style={{padding:10}}>
+          <Text style={{color:colors.textColor, fontSize:getFontSize(width, "other") - 2}}>Category</Text>
+          <SelectedComponents item={categoryList} setSelected={setCategory}/>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const style = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:colors.background
-    }
-})
-export default AddBudgetPage
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  input: {
+    padding: 10,
+    gap: 10,
+  },
+  textInput: {
+    backgroundColor: colors.background,
+    borderWidth: 1,
+    borderColor: colors.grey,
+    padding: 5,
+    borderRadius: 10,
+    color: colors.textColor,
+    fontSize: getFontSize(width, "min"),
+    height: height * 0.07,
+  },
+});
+export default AddBudgetPage;
